06aea7a649b6821c4d29ef6c2b8c476607686acb
Ashutosh Sharan <being.ashutosh16.20@gmail.com>
2025-03-29 12:32:36 +0530

    Background to foreground contrast fixes

--- components/global/VideoModal.tsx
+++ components/global/VideoModal.tsx
@@ -3,45 +3,56 @@ import {
     DialogContent,
     DialogTitle,
     DialogTrigger,
-} from "@/components/ui/dialog"
-import { cn } from "@/lib/utils"
-import { VideoModalTriggerProps } from "@/types"
-import Image from "next/image"
+  } from "@/components/ui/dialog";
+  import { cn } from "@/lib/utils";
+  import { VideoModalTriggerProps } from "@/types";
+  import Image from "next/image";
 
-  export function VideoModal({ image, videoUrl, className }: VideoModalTriggerProps) {
+  export function VideoModal({
+    image,
+    videoUrl,
+    className,
+  }: VideoModalTriggerProps) {
     return (
-        <Dialog>
-        <DialogTrigger asChild className={cn(className)}>
-          <div className="relative rounded-xl aspect-square w-[180px] h-[180px] md:w-[220px] md:h-[220px] lg:w-[280px] lg:h-[280px] overflow-hidden">
-            <Image
-              src={"/player-thumb-overlay.svg"}
-              alt="Play button image overlay"
-              width={70}
-              height={70}
-              className=" top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-pointer z-10 absolute pointer-events-none"
-            />
+      <Dialog>
+        <DialogTrigger asChild>
+          <div
+            className={cn(
+              "relative rounded-xl aspect-square w-[180px] h-[180px] md:w-[220px] md:h-[220px] lg:w-[280px] lg:h-[280px] overflow-hidden group cursor-pointer",
+              className
+            )}
+            title="Play Patient Testimonial Video"
+          >
             <Image
               src={image}
               alt="Video thumbnail"
               width={280}
               height={180}
-              className=" size-full object-cover hover:scale-105 transition-transform duration-300 shadow-blob"
+              className="size-full object-cover transition-transform duration-300 group-hover:scale-105"
+            />
+            <Image
+              src="/player-thumb-overlay.svg"
+              alt="Play button overlay"
+              width={70}
+              height={70}
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 pointer-events-none"
             />
           </div>
         </DialogTrigger>
-        <DialogContent className="rounded-2xl p-0 m-0 overflow-hidden min-w-[80vw] max-w-5xl w-full">
+
+        <DialogContent className="rounded-2xl p-0 overflow-hidden min-w-[90vw] max-w-5xl w-full">
           <DialogTitle className="sr-only">Patient Testimonial Video</DialogTitle>
           <div className="aspect-video w-full">
             <iframe
               className="w-full h-full"
               src={videoUrl}
               title="Patient Testimonial Video"
-              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             />
+            {/* Optional: If autoplay is needed only on modal open, use a state and conditional rendering */}
           </div>
         </DialogContent>
       </Dialog>
-    )
+    );
   }
