1d4842491fd651a97f312a92b82c3b29e98f0745
Ashutosh Sharan <being.ashutosh16.20@gmail.com>
2025-04-02 11:39:49 +0530

    Adjusted the widths of the elements to match a better modern UI centric approach

--- components/homepage/FaqAccordion.tsx
+++ components/homepage/FaqAccordion.tsx
@@ -12,53 +12,55 @@ import { MinusCircle, PlusCircle } from "lucide-react";
 import { useState } from "react";
 
 interface FAQItem {
-  question: string;
-  answer: string;
+    question: string;
+    answer: string;
 }
 
 interface FaqAccordionProps {
-  items: FAQItem[];
+    items: FAQItem[];
 }
 
 export default function FaqAccordion({ items }: FaqAccordionProps) {
-  const [openItem, setOpenItem] = useState<string | null>(null);
+    const [openItem, setOpenItem] = useState<string | null>(null);
 
-  const handleToggle = (itemValue: string) => {
-    setOpenItem((prev) => (prev === itemValue ? null : itemValue));
-  };
+    const handleToggle = (itemValue: string) => {
+        setOpenItem((prev) => (prev === itemValue ? null : itemValue));
+    };
 
-  return (
-    <Accordion type="single" collapsible className="w-full space-y-3">
-      {items.map((item, index) => {
-        const value = `item-${index}`;
-        const isOpen = openItem === value;
+    return (
+        <Accordion type="single" collapsible className="w-full space-y-3">
+            {items.map((item, index) => {
+                const value = `item-${index}`;
+                const isOpen = openItem === value;
 
-        return (
-            <AccordionItem
-            value={value}
-            key={index}
-            className={cn(
-              "rounded-lg bg-muted/30 !border !border-indigo-200 focus-visible:!ring-0",
-              isOpen && "shadow-expanded"
-            )}
-          >
-            <AccordionTrigger
-              onClick={() => handleToggle(value)}
-              className={cn("flex  justify-between items-center px-4 py-3 text-left text-base lg:text-lg font-medium hover:no-underline", isOpen ? "text-primary" : "text-foreground")}
-            >
-                <span className="w-full text-sm md:text-base  ">
-                    {item.question}
-                </span>
-              <span className="w-max">
-                {isOpen ? <MinusCircle className="size-7 md:size-8 text-primary" /> : <PlusCircle className="size-7 md:size-8 text-primary" />}
-              </span>
-            </AccordionTrigger>
-            <AccordionContent className="px-4  text-gray-800">
-              {item.answer}
-            </AccordionContent>
-          </AccordionItem>
-        );
-      })}
-    </Accordion>
-  );
+                return (
+                    <AccordionItem
+                        value={value}
+                        key={index}
+                        className={cn(
+                            "rounded-lg bg-muted/30 !border !border-indigo-200 focus-visible:!ring-0",
+                            isOpen && "shadow-expanded"
+                        )}
+                    >
+                        <AccordionTrigger
+                            onClick={() => handleToggle(value)}
+                            className={cn("flex  justify-between items-center px-4 py-3 text-left text-base lg:text-lg font-medium hover:no-underline", isOpen ? "text-primary" : "text-foreground")}
+                        >
+                            <span className="w-full text-sm md:text-base  ">
+                                {item.question}
+                            </span>
+                            <span className="w-max">
+                                {isOpen ? <MinusCircle className="size-7 md:size-8 text-primary" /> : <PlusCircle className="size-7 md:size-8 text-primary" />}
+                            </span>
+                        </AccordionTrigger>
+                        <AccordionContent className="px-4  text-gray-800 text-base">
+                            <p className="max-w-3/4">
+                                {item.answer}
+                            </p>
+                        </AccordionContent>
+                    </AccordionItem>
+                );
+            })}
+        </Accordion>
+    );
 }
